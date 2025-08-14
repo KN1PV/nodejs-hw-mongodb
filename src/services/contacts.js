1b1a1a1460af7bc9e21b1375407f72b02aae76ec
Vadym <vadim.drinevskiy@gmail.com>
2025-08-14 17:41:56 +0300

    Added sorting by field and order for GET /contacts

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,38 @@
 import { Contact } from "../db/models/contact.js"
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constants/index.js";
+import createHttpError from "http-errors";
 
-export const getContacts = async () => {
-    const contacts = await Contact.find();
-    return contacts;
+export const getContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+    if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+    };
+  if (typeof filter.isFavourite !== 'undefined') {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+    };
+
+  const [contactsCount, contacts] = await Promise.all([
+    Contact.find().merge(contactsQuery).countDocuments(),
+    contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (userId) => {
@@ -23,5 +53,9 @@ export const patchContact = async (contactId, payload) => {
 };
 
 export const deleteContactById = async (contactId) => {
-    await Contact.findByIdAndDelete(contactId);
+    const contact = await Contact.findByIdAndDelete(contactId);
+
+    if (!contact) {
+        throw createHttpError(404, 'Contact not found');
+    }
 }
