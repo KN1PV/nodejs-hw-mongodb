d56418ae1406f02935fcea1fb4d58fa5a6a392a7
Vadym <vadim.drinevskiy@gmail.com>
2025-08-17 23:22:49 +0300

    fix validation error message

--- src/middlewares/validateBodyMiddleware.js
+++ src/middlewares/validateBodyMiddleware.js
@@ -1,19 +1,8 @@
-import createHttpError from 'http-errors'
-
-export const validateBody = (schema) => {
-  return async (req, res, next) => {
-    try {
-      await schema.validateAsync(req.body, {
-        allowUnknown: false,
-        abortEarly: false,
-        convert: false,
-      });
-      next();
-    } catch (err) {
-      next(createHttpError(400, {
-        message: 'Validation error',
-        errors: err.details,
-      }));
-    }
-  };
+export const validateBody = (schema) => async (req, res, next) => {
+  await schema.validateAsync(req.body, {
+    allowUnknown: false,
+    abortEarly: false,
+    convert: false,
+  });
+  next();
 };
