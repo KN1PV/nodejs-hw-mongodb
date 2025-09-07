4cc7b1a78659493c9ef07882d83a024b37e00cc8
Vadym <vadim.drinevskiy@gmail.com>
2025-09-07 23:59:55 +0300

    fix bugs

--- src/middlewares/validateBodyMiddleware.js
+++ src/middlewares/validateBodyMiddleware.js
@@ -2,7 +2,7 @@ export const validateBody = (schema) => async (req, res, next) => {
   await schema.validateAsync(req.body, {
     allowUnknown: false,
     abortEarly: false,
-    convert: false,
+    convert: true,
   });
   next();
 };
